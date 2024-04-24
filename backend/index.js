e59c60e5ede6a42bbf895365e251fdb6a10129f4
Ikramul-m <i20727172@gmail.com>
2024-04-24 15:38:03 +0530

    Get all Book from mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 // import express, { response } from "express";
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/BookModel.js";
@@ -14,7 +14,10 @@ app.get("/", (request, response) => {
   return response.status(234).send("Welcome To MERN Stack Tutorial!!!");
 });
 
-// Route for save a new book
+
+
+
+// Route for save a new book POST
 app.post("/books", async (request, response) => {
   try {
     if (
@@ -42,10 +45,31 @@ app.post("/books", async (request, response) => {
 });
 
 
+
+
+
+//Routs for GET all Databases from
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+
+    return response.status(200).send({
+      count: books.length,
+      data: books
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+
+
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
-    console.log('App connected to database');
+    console.log("App connected to database");
     app.listen(PORT, () => {
       console.log(`App is listening to port: ${PORT}`);
     });
