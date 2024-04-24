51f14594b179c17936a45294ac46cf54685a1dc8
Ikramul-m <i20727172@gmail.com>
2024-04-24 22:25:46 +0530

    update

--- frontend/src/Page/Home.jsx
+++ frontend/src/Page/Home.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
+import Spinner from "../components/Spinner";
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div>Home</div>;
+};
 
-export default Home
+export default Home;
